296b116509b04db03ef3b29bc95b334364f19e52
Grzegorz Gebala <grzegorz.gebala@yahoo.com>
2019-01-05 11:16:55 +0100

    Final commit

---
+++ actions.js
@@ -0,0 +1,46 @@
+import uuid from uuid;
+
+export const ADD_COMMENT = 'ADD_COMMENT';
+export const EDIT_COMMENT = 'EDIT_COMMENT';
+export const REMOVE_COMMENT = 'REMOVE_COMMENT';
+export const THUMB_UP_COMMENT = 'THUMB_UP_COMMENT';
+export const THUMB_DOWN_COMMENT = 'THUMB_DOWN_COMMENT';
+
+function addComment(text) {
+    return {
+        type: ADD_COMMENT,
+        text,
+        id: uuid.v4()
+    }
+}
+
+function editComment(id, text) {
+    return {
+        type: EDIT_COMMENT,
+        id,
+        text: text        
+    }
+}
+
+function removeComment(id) {
+    return {
+        type: REMOVE_COMMENT,
+        id        
+    }
+}
+
+function thumbUpComment(id, fingerUp) {
+    return {
+        type: THUMB_UP_COMMENT,
+        id,
+        fingerUp: fingerUp++
+    }
+}
+
+function thumbDownComment(id, fingerDown) {
+    return {
+        type: THUMB_DOWN_COMMENT,
+        id,
+        fingerDown: fingerDown--
+    }
+}
